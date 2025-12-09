e0e65dabca7fac1ed3c44e250fb35593b0404201
Jonathan Ringer <jonringer117@gmail.com>
2025-12-08 21:14:38 -0800

    wip: initial cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "ekapkgs-update")]
+#[command(about = "Update ekapkgs packages", long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Run the update process
+    Run {
+        /// File or directory to process
+        #[arg(short, long)]
+        file: Option<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Run { file } => {
+            if let Some(file) = file {
+                println!("Running update with file: {}", file);
+            } else {
+                println!("Running update with no file specified");
+            }
+        }
+    }
 }
