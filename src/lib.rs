6e8f2d6e2b9fdc44f58d78b1c826fba8280c1a10
Jonathan Ringer <jonringer117@gmail.com>
2026-05-08 17:28:47 -0700

    Provid lib.rs shim

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+//! Library interface for ekapkgs-update
+//!
+//! This library target exists primarily to support doctests in the module
+//! documentation. The primary interface is the binary (`src/main.rs`).
+
+pub mod cachix;
+pub mod cli;
+pub mod commands;
+pub mod config;
+pub mod cve;
+pub mod database;
+pub mod directory_diff;
+pub mod git;
+pub mod github;
+pub mod gitlab;
+pub mod hash_discovery;
+pub mod init;
+pub mod nix;
+pub mod package;
+pub mod paths;
+pub mod pypi;
+pub mod repology;
+pub mod rewrite;
+pub mod variant_strategy;
+pub mod vcs_sources;
+
+#[cfg(test)]
+mod tests;
